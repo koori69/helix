35e68fec22a4b2022ce3029f811de34dd2da8954
Jyo <koori69@github.com>
2018-02-04 11:01:21 +0800

    Expoとantd-moblileの不具合を解決したため、antd-mobileに変更した iconを変更した

--- screens/DictScreen.js
+++ screens/DictScreen.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import { View, Platform, TextInput } from 'react-native';
-import { ButtonGroup, FormLabel, FormInput, Button } from 'react-native-elements';
-import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { View, Text, Platform } from 'react-native';
+import { InputItem, Radio, List, WhiteSpace, Tag, Button } from 'antd-mobile';
+import { createForm } from 'rc-form';
+import { Ionicons } from '@expo/vector-icons';
 import { WebBrowser } from 'expo';
 
+const RadioItem = Radio.RadioItem;
 const data = [
     { value: 0, label: '日中', extra: 'jc' },
     { value: 1, label: '中日', extra: 'cj' },
 ];
 
-export default class DictScreen extends Component {
+class Dict extends Component {
   static navigationOptions = {
     title: 'Dict',
   };
@@ -19,46 +21,54 @@ export default class DictScreen extends Component {
     this.state = {
       selected: 0,
     };
-    this.onChange = this.onChange.bind(this);
+    // this.onChange = this.onChange.bind(this);
+    // this.onSearch = this.onSearch(this);
   }
 
-  onChange(selectedIndex) {
-    console.log(selectedIndex);
-    this.setState({ selected: selectedIndex });
+  onChange(value) {
+    console.log(value);
+    this.setState({ selected: value });
   }
 
-  onSearch = () => {
-    console.log('in onSearch');
+  onSearch() {
     const baseUrl = 'https://dict.hjenglish.com/jp';
     const lang = this.state.selected;
-    const word = this.state.word;
-    console.log(word);
+    const word = this.props.form.getFieldValue('word');
     const url = `${baseUrl}/${data[lang].extra}/${encodeURI(word)}`;
     console.log(url);
     WebBrowser.openBrowserAsync(url);
-  };
+  }
 
   render() {
-    const buttons = [];
-    data.map(i => i.label).forEach(t => buttons.push(t));
-    console.log(buttons);
-    const { selected } = this.state;
+    const { getFieldProps } = this.props.form;
     return (
       <View>
-        <FormLabel>Word</FormLabel>
-        <TextInput onChangeText={(word) => { console.log(word); this.setState({ word }); }} />
-        <ButtonGroup
-          onPress={selectedIndex => this.onChange(selectedIndex)}
-          selectedIndex={selected}
-          buttons={buttons}
-          selectedButtonStyle={{ backgroundColor: 'green' }}
-        />
-        <Button
-          raised
-          onPress={this.onSearch}
-          title={'Search'}
-        />
+        <List>
+          <InputItem
+            {...getFieldProps('word')}
+            placeholder={'search word'}
+          >Word:
+          </InputItem>
+          <WhiteSpace />
+          {
+                data.map(i => (
+                  <RadioItem
+                    key={i.value}
+                    checked={this.state.selected === i.value}
+                    onChange={() => this.onChange(i.value)}
+                  >
+                    {i.label}
+                  </RadioItem>
+                ))
+            }
+          <Button type="primary" onClick={() => this.onSearch()}>
+              Search
+            <Ionicons name={Platform.OS === 'ios' ? 'ios-search' : 'md-search'} size={26} />
+          </Button>
+        </List>
       </View>
     );
   }
 }
+
+export default createForm()(Dict);
