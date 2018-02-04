e5a164643ccddf4df3ddd052065eecf7b30bb3ed
Jyo <koori69@github.com>
2018-02-04 16:47:43 +0800

    antd-mobileを使用すると、公開時、react-domを見つからない不具合がある。 解決方法はreact-domを明らかにインストールする。

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { TabNavigator, TabBarBottom } from 'react-navigation';
-import { Platform } from 'react-native';
 import { Ionicons, MaterialCommunityIcons, Feather } from '@expo/vector-icons';
 import { DictScreen, MovieScreen, TvScreen } from '../screens/';
 import Colors from '../constants/Colors';
@@ -22,19 +21,6 @@ export default TabNavigator(
         const { routeName } = navigation.state;
         let iconName;
         switch (routeName) {
-          case 'Home':
-            iconName =
-                            Platform.OS === 'ios'
-                                ? `ios-information-circle${focused ? '' : '-outline'}`
-                                : 'md-information-circle';
-            break;
-          case 'Links':
-            iconName = Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link';
-            break;
-          case 'Settings':
-            iconName =
-                            Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options';
-            break;
           case 'Dict':
             iconName = 'dictionary';
             break;
